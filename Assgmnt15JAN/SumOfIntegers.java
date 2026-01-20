bb3a3ecb524a872fe9c9d77348df6762ba9ae69e
Pranjul <pranjul.here@gmail.com>
2026-01-20 16:54:17 +0530

    fixed SumOfIntegers

--- Assgmnt15JAN/SumOfIntegers.java
+++ Assgmnt15JAN/SumOfIntegers.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class SumofIntegers {
+public class SumOfIntegers {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("How many numbers do you want to add?: ");
+        System.out.print("How many numbers do you want to add?: ");
         int count = scanner.nextInt();
 
         int sum = 0;
 
         for (int i = 1; i <= count; i++) {
-            System.out.println("Enter number " + i + ": ");
+            System.out.print("Enter number " + i + ": ");
             int number = scanner.nextInt();
             sum += number;
         }
