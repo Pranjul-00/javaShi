c35507b6ba23e33f249d9ac21a686bd87f8b0fa3
Pranjul <pranjul.here@gmail.com>
2026-01-29 11:53:34 +0530

    added 2 pattern files

---
+++ Assgmnt15JAN/pattern2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class pattern2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+
+}
